17897b2a822fae8c2be16b2245a7ed2eb2671fed
rr2210 <razvanrosie@yahoo.com>
2013-11-19 20:30:33 +0000

    add classes in the utils

---
+++ AcmeTelecom/src/com/acmetelecom/utils/LineItem.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acmetelecom.utils;
+
+import com.acmetelecom.Call;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author rr2210
+ */
+    public class LineItem 
+    {
+        private Call call;
+        private BigDecimal callCost;
+
+        public LineItem(Call call, BigDecimal callCost) {
+            this.call = call;
+            this.callCost = callCost;
+        }
+
+        public String date() {
+            return call.date();
+        }
+
+        public String callee() {
+            return call.callee();
+        }
+
+        public String durationMinutes() {
+            return "" + call.durationSeconds() / 60 + ":" + String.format("%02d", call.durationSeconds() % 60);
+        }
+
+        public BigDecimal cost() {
+            return callCost;
+        }
+    }
--- AcmeTelecom/src/com/acmetelecom/utils/MoneyFormatter.java
+++ AcmeTelecom/src/com/acmetelecom/utils/MoneyFormatter.java
@@ -2,8 +2,15 @@ package com.acmetelecom.utils;
 
 import java.math.BigDecimal;
 
-public class MoneyFormatter {
-    public static String penceToPounds(BigDecimal pence) {
+public class MoneyFormatter 
+{
+    /**
+     * 
+     * @param pence
+     * @return 
+     */
+    public static String penceToPounds(BigDecimal pence) 
+    {
         BigDecimal pounds = pence.divide(new BigDecimal(100));
         return String.format("%.2f", pounds.doubleValue());
     }
