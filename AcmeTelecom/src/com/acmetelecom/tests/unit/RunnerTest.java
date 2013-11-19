af83211cc69783aac789b8eb6ec2dda4f025fcd7
Marius Telespan <Marius@dyn1239-198.wlan.ic.ac.uk>
2013-11-19 19:57:35 +0000

    create unit package

---
+++ AcmeTelecom/src/com/acmetelecom/tests/unit/RunnerTest.java
@@ -0,0 +1,14 @@
+package com.acmetelecom.tests.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RunnerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
