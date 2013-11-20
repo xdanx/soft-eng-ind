1479a02b3345f8e71947a4c6ab48bd5da346378f
rr2210 <razvanrosie@yahoo.com>
2013-11-20 18:05:09 +0000

    add test for DaytimePeakPeriod

--- AcmeTelecom/src/com/acmetelecom/utils/DaytimePeakPeriod.java
+++ AcmeTelecom/src/com/acmetelecom/utils/DaytimePeakPeriod.java
@@ -42,10 +42,16 @@ public class DaytimePeakPeriod
         return answer;
     }
     
+    
+    /*
+     * Given an input date, the function returns the next lower, either peak
+     * or off peak.
+     * 
+     * e.g. getNextLowerDelimiter(18 Nov, 18.51) will return 18 Nov, 7.00 
+     */    
     public static DateTime getNextLowerDelimiter(DateTime dt)
     {       
     	DateTime answer;
-
         if(dt.getHourOfDay() >= 7  && dt.getHourOfDay() < 19)
         {
             answer = dt.withHourOfDay(7).withMinuteOfHour(0).withSecondOfMinute(0);
@@ -71,7 +77,6 @@ public class DaytimePeakPeriod
     * toStandardSeconds() Throws:
     * UnsupportedOperationException - if the period contains years or months 
     * ArithmeticException - if the number of seconds is too large to be represented
-    * 
     * 
     */
    public static int getDifferenceInSeconds(DateTime start, DateTime end)
