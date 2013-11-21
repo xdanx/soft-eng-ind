61a16047128786837a9686aac8812c329a6a8c25
Dan Octavian <dan@danpad.(none)>
2013-11-21 13:58:47 +0000

    added runner

---
+++ AcmeTelecom/src/com/acmetelecom/runner/BillingSystemManualTest.java
@@ -0,0 +1,35 @@
+package com.acmetelecom.runner;
+
+import java.util.List;
+
+import com.acmetelecom.ArrayCallLog;
+import com.acmetelecom.BillCreator;
+import com.acmetelecom.CallLog;
+import com.acmetelecom.HtmlBillPrinter;
+import com.acmetelecom.customer.CentralCustomerDatabase;
+import com.acmetelecom.customer.CentralTariffDatabase;
+import com.acmetelecom.customer.CustomerDatabase;
+
+public class BillingSystemManualTest {
+	public static void main(String[] args) {
+		CallLog log = new ArrayCallLog();
+		long time = 12 * 3600 * 1000;
+		log.logCallStart("447722113434", "447766511332", time);
+		
+		time += 20000;
+		log.logCallEnd("447722113434", "447766511332", time);
+		log.logCallStart("447722113434", "447711111111", time);
+		
+		time += 30000;
+		log.logCallEnd("447722113434", "447711111111", time);
+		log.logCallStart("447777765432", "447711111111", time);
+		time += 3600000;
+		log.logCallEnd("447777765432", "447711111111", time);
+		
+		BillCreator billCreator = new BillCreator(new HtmlBillPrinter());
+		List<String> bills = billCreator.createBills(log, CentralCustomerDatabase.getInstance());
+		for (String bill : bills) {
+			System.out.println(bill);
+		}
+	}
+}
